f122bc51d20e38e2e4c30063feee64abd65783f4
Mark Macaraeg <markmcrg9@gmail.com>
2024-07-12 01:52:46 +0800

    Added initial files and page for accredited orgs

---
+++ helpers.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import requests
+from requests.auth import HTTPBasicAuth
+
+def fetch_data(url):
+    PUBLIC_KEY = st.secrets.tidb_keys.public_key
+    PRIVATE_KEY = st.secrets.tidb_keys.private_key
+    response = requests.get(url, auth=HTTPBasicAuth(PUBLIC_KEY, PRIVATE_KEY))
+    return response.json()
