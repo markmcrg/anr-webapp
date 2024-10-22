a591c6214a153463f6ca74e82e226dfe951e7b77
Mark Macaraeg <markmcrg9@gmail.com>
2024-10-22 09:58:23 +0800

    Added user management page for en banc

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ with st.sidebar:
                 sac.MenuItem('Admin Tools', disabled=True),
                 sac.MenuItem(type='divider'),
                 sac.MenuItem('Assign Organizations', icon='bi bi-person-check'),
+                sac.MenuItem("User Management", icon='bi bi-person-lines-fill'),
                 sac.MenuItem('View Document Status', icon='bi bi-file-earmark-text'),
             ], open_all=False)
     page_bg_img = """
@@ -353,29 +354,3 @@ st.markdown("""
 
             """
             ,unsafe_allow_html=True)
-
-# with st.form("login_form"):
-#     username = st.text_input("**Username**", placeholder="pupsccosoa")
-#     password = st.text_input("**Password**", type="password")
-#     submitted = st.form_submit_button("Login", type="primary")
-    
-#     if submitted:
-#         user_data = fetch_data('https://ap-southeast-1.data.tidbcloud.com/api/v1beta/app/dataapp-SxHAXFax/endpoint/users')['data']
-#         user_data_df = pd.DataFrame(
-#             user_data["rows"], columns=[col["col"] for col in user_data["columns"]]
-#         )
-#         matching_user = user_data_df[(user_data_df['username'] == username) & (user_data_df['password'] == password)]
-        
-#         if not matching_user.empty:
-#             st.success("Login successful.")
-#             org_name = matching_user['org_name'].values[0]
-            
-#             # Set session state variables
-#             st.session_state['authentication_status'] = True
-#             st.session_state['username'] = username
-#             st.session_state['name'] = org_name
-            
-#             st.rerun()
-            
-#         else:
-#             st.error("Username or password is incorrect. Please try again.")
